44d69ed081915e2a5d920ca4d5fc3ab00fa2eed9
Matthis Ehrhardt <ehrhardt.matthis@gmail.com>
2023-11-14 09:08:32 +0100

    working for question one

--- big_data_9.py
+++ big_data_9.py
@@ -1,39 +1,59 @@
 import numpy as np
 import pandas as pd
-import pprint
-import os
+import random
+import sys
 
-from pandas.io.formats.printing import pprint_thing
+#np.set_printoptions(threshold=sys.maxsize)
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_colwidth', None)
 
 
-def get_data(sig, n):
-    data_raw = pd.read_csv('/home/admin/Uni/ub_code/wheatX.csv')
-    data = {}
-    values = []
-    for x in data_raw:
-        values = str(x).split(".")
-        values = [value_.replace("\"", "").split(";") for value_ in values]
+def get_data(s, p: int | None = None):
+    data_raw = pd.read_csv('/home/admin/Uni/ub_code/wheatX.csv', sep=";")
 
-    for item in values:
-        if len(item) <= 1:
-            continue
-        name = item[1]
-        value = item[0]
-        if name not in data:
-            data[name] = []
-        data[name].append((float)(value) if value != '' else np.nan)
+    # get head of table to seperate x and y
+    # there are 2 possible start strings for each item in head "c" and "wPt"
+    head = [item[0] for item in data_raw.items()]
+    values = np.transpose(data_raw.values)
+    x_beta, y = [], []
+    for i, name in enumerate(head):  # sort input data into x and y values
+        if str(name)[0] == "c":
+            x_beta.append(values[i])
+        elif str(name)[0] == "w":
+            y.append(values[i])
 
-    pprint.pprint(data)
+    x_beta = np.array(x_beta).transpose()
+    y = np.array(y).transpose()  # transfrom to np.array
 
-#   data = np.array(data)
-#   mean_x = np.mean(data)
-#   sig = np.sqrt(1 / (len(X) - 1) * sum([(x - mean_x) ** 2 for x in X]))
-#   print(sig)
-#   eps = np.random.normal(0, sig * np.identity(n))
+    if p is not None:  # check if some columns should be filtered out
+        rand_indexes = np.random.choice(x_beta.shape[1], p, replace=False)
+        x_beta = x_beta[:, rand_indexes]
+
+    # calculate epsilon based on shape and standart deviation
+    sig = np.var(x_beta, ddof=1)  # ddof=1 for empirical variance
+    eps = np.random.normal(0, s * sig, size=(x_beta.shape[0], 1))
+    return y, x_beta, eps
 
 
-get_data(1,100)
+def get_test_data(alpha: float):
+    if alpha > 1:
+        raise ValueError("Alpha needs to be below one")
+
+    y, x_beta, eps = get_data(1)
+    num_individuals = (int)(y.shape[1] * alpha)
+    rand_indexes = np.random.choice(x_beta.shape[0],
+                                    num_individuals,
+                                    replace=False
+                                    )
+    x_beta = x_beta[:, rand_indexes]
+    eps = eps[:, rand_indexes]
+    y = y[:, rand_indexes]
+    return y, x_beta, eps, rand_indexes
+
+
+# Question 1
+y, x_beta, eps = get_data(s=2, p=10)
+print(y.shape, x_beta.shape, eps.shape)
+
+# Question 2
+y, x_beta, eps = get_test_data(s=2, p=10)
